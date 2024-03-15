9996563c4452a08f47138d5ffabe9b52a529e942
devenv1390 <1223544853@qq.com>
2024-03-16 00:56:10 +0800

    更换国内源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -8,6 +12,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/google") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public") }
+        maven { url = uri("https://maven.aliyun.com/nexus/content/repositories/jcenter") }
         google()
         mavenCentral()
     }
